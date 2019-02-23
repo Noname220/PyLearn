1eb74b56ffc48ff340bde75e7ba0ab573b65e543
Noname220 <pavluca2005@gmail.com>
2019-02-23 07:32:47 +0200

    Obnova no e pomylka

--- Homework/Soldier0.1.py
+++ Homework/Soldier0.1.py
@@ -8,10 +8,21 @@ class Soldaten:
         discription = (self.name +" Xp is "+ str (self.xp) +" Uron is "+ str (self.uron))
         print(discription)
 
+    def Atack(self, target: "Soldaten"):
+        target.xp -= self.uron
+
 
 mySoldaten = Soldaten("Kulak",25)
 mySoldaten2 = Soldaten("Bochka",20)
 
 mySoldaten.show_Soldaten()
 mySoldaten2.show_Soldaten()
+
+import random
+n = random.choice(1,2)
+if (n == 1):
+   mySoldaten.atack(mySoldaten2)
+else:
+   mySoldaten2.atack(mySoldaten)
+
 
